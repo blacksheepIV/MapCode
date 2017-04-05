2a786451693601c7007064fdb0c1d9a47ff19ca6
mahdavipanah <h.mahdavipanah@gmail.com>
2017-04-05 10:49:57 +0430

    Rename index.html to main.html

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public/')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/main.html'));
 });
 
 module.exports = app;
