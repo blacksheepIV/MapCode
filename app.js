5351e85274c5ec2d6e28555df2da36982164888d
mahdavipanah <h.mahdavipanah@gmail.com>
2017-04-05 11:15:35 +0430

    Insert API HREF in main html file

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var path = require('path');
+var fs = require('fs');
 
 var express = require('express');
 var helmet = require('helmet');
+var mustache = require('mustache');
 
+
+require('dotenv').config({path: path.join(__dirname, '/.env')});
+console.log(process.env.API_HREF);
 
 var app = express();
 
@@ -10,8 +15,11 @@ app.use(helmet());
 
 app.use(express.static(path.join(__dirname, 'public/')));
 
+var mainHtml = fs.readFileSync(path.join(__dirname + '/public/main.html'), 'utf8');
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/public/main.html'));
+    res.send(mustache.render(mainHtml, {
+        apiHref: process.env.API_HREF
+    }));
 });
 
 module.exports = app;
