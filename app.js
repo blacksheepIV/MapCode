fb2eaf3ed89074446163d0c538be4f9c1460ad11
mahdavipanah <h.mahdavipanah@gmail.com>
2017-04-06 11:40:14 +0430

    Make main.html file parsinng asynchronous and Add watcher for it

--- app.js
+++ app.js
@@ -14,12 +14,42 @@ app.use('/api', require('./api'));
 
 app.use(express.static(path.join(__dirname, 'public/')));
 
+var mainHtmlAbPath = path.join(__dirname + '/public/main.html');
+var mainHtml = '';
 
-var mainHtml = fs.readFileSync(path.join(__dirname + '/public/main.html'), 'utf8');
+/*
+ Reads public/main.html file and parses it with mustache package and then saves it
+ in mainHtml variable.
+ */
+function parseMainHtml() {
+    fs.readFile(mainHtmlAbPath, {encoding: 'utf-8'}, function(err, fileContent) {
+        if (!err) {
+            mainHtml = mustache.render(fileContent, {
+                apiHref: process.env.API_HREF
+            });
+        }
+        else {
+            console.error("Can't read '%s'", mainHtmlAbPath);
+        }
+    });
+}
+parseMainHtml();
+
 app.get('/*', function (req, res) {
-    res.send(mustache.render(mainHtml, {
-        apiHref: process.env.API_HREF
-    }));
+    res.send(mainHtml);
 });
+
+/*
+ Watches for changes in public/main.html file.
+ */
+function watchMainHtml() {
+    fs.watch(mainHtmlAbPath, function(eventType) {
+        if (eventType == 'change') {
+            parseMainHtml();
+        }
+        watchMainHtml();
+    });
+}
+watchMainHtml();
 
 module.exports = app;
