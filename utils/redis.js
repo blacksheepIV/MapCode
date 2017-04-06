6b6a17843299cfa1b1ab0cb2d6c8ac05c0191a1a
mahdavipanah <h.mahdavipanah@gmail.com>
2017-04-06 16:11:06 +0430

    Add redis error handler

--- utils/redis.js
+++ utils/redis.js
@@ -3,5 +3,9 @@ var redis = require('redis');
 
 var redis_client = redis.createClient();
 
+redis_client.on("error", function(err) {
+    console.error("Redis Error : " + err);
+});
+
 
 module.exports = redis_client;
