c139f338368605be84544be2aeacfd2983e6cb58
Forough Fallah <forough.fallahfar@gmail.com>
2017-04-09 16:39:20 +0430

    Update controller

---
+++ public/js/controllers/userCtrl.js
@@ -0,0 +1,28 @@
+/**
+ * Created by blackSheep on 09-Apr-17.
+ */
+var userCtrl = function($scope){
+    $scope.initVars = function(){
+        $scope.investigate = false; // user's not been investigated and approved yet
+        $scope.user = {
+            name: '',
+            melli_code: 0,
+            email: '',
+            date: '',
+            mobile_phone: '',
+            phone: '',
+            username: '',
+            password: '',
+            passRepeat:'',
+            address: '',
+            description: '',
+            isRecommended: false,
+            recommender: 0,
+            type: '',
+            code: '',
+            credit: 0,
+            bonus: 0
+        }
+        //u get user's type from database;store it in a variable and then the user's type gotta be equal to that value
+    }
+}//end of userCtrl controller
