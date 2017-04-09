a7bdc21020899ee6d9f1569830caf7c4ca4f6b42
Forough Fallah <forough.fallahfar@gmail.com>
2017-04-09 18:03:01 +0430

    angular_uploadInstall userPanelchanges restored

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,9 @@
 /**
  * Created by blackSheep on 27-Mar-17.
  */
-var mapCodeApp = angular.module('mapCodeApp',['ngMaterial','ngRoute','ngResource','ngMessages']);
+var mapCodeApp = angular.module('mapCodeApp',['ngMaterial','ngRoute','ngResource','ngMessages','lr.upload']);
     mapCodeApp.controller('loginCtrl',loginCtrl)
         .controller('registerCtrl',registerCtrl)
         .controller('mainCtrl',mainCtrl)
+        .controller('userCtrl',userCtrl)
         .service('regService',regService);
