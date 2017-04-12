9a801e9f556c8a86fda9a37923bc8e6d95cda7b6
mahdavipanah <h.mahdavipanah@gmail.com>
2017-04-12 15:09:27 +0430

    Config log options for PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  apps : [
-    {
-      name      : 'ProjectM',
-      script    : 'bin/www',
-      env: {
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
+    apps: [
+        {
+            name: 'ProjectM',
+            script: 'bin/www',
+            env: {},
+            env_production: {
+                NODE_ENV: 'production'
+            },
+            "merge_logs": true,
+            "log_date_format": "YYYY-MM-DD HH:mm Z"
+        }
+    ],
+    deploy: {
+        production: {
+            user: 'arshingolabchi1',
+            host: '188.40.89.142',
+            ref: 'origin/master',
+            repo: 'git@gitlab.com:ProjectM/src.git',
+            path: '/var/www/vhosts/mapcode.ir',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+        }
     }
-  ],
-  deploy : {
-    production : {
-      user : 'arshingolabchi1',
-      host : '188.40.89.142',
-      ref  : 'origin/master',
-      repo : 'git@gitlab.com:ProjectM/src.git',
-      path : '/var/www/vhosts/mapcode.ir',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
 };
