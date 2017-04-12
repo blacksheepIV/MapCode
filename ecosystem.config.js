91c6a60d3b04e10d12b551ac500e28d8b1032c99
mahdavipanah <h.mahdavipanah@gmail.com>
2017-04-12 13:35:54 +0430

    Add PM2 ecosystem.config.json file

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [
+    {
+      name      : 'PorjectM',
+      script    : 'bin/www',
+      env: {
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+  deploy : {
+    production : {
+      user : 'arshingolabchi1',
+      host : '188.40.89.142',
+      ref  : 'origin/master',
+      repo : 'git@gitlab.com:ProjectM/src.git',
+      path : '/var/www/vhosts/mapcode.ir',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
