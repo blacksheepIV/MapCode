f6ffbfd0f006996f5cadf2f438a9e4a8ac116670
mahdavipanah <h.mahdavipanah@gmail.com>
2017-04-12 17:47:20 +0430

    Add support for Redis password authentication

--- utils/redis.js
+++ utils/redis.js
@@ -7,5 +7,10 @@ redis_client.on("error", function(err) {
     console.error("Redis Error : " + err);
 });
 
+// If REDIS_PASSWORD variable is defined inside .env file
+if (typeof process.env.REDIS_PASSWORD !== 'undefined') {
+    // Authenticate Redis
+    redis_client.auth(process.env.REDIS_PASSWORD);
+}
 
 module.exports = redis_client;
