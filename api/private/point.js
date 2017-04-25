48eb31f42228295ce2a35a94d0770dbacd881094
mahdavipanah <h.mahdavipanah@gmail.com>
2017-04-25 22:30:08 +0430

    Add Point submission API docs

--- api/private/point.js
+++ api/private/point.js
@@ -4,7 +4,72 @@ var pointModel = require('../../models/point');
 
 
 router.route('/point')
-// TODO: Point POST API docs
+/**
+ * @api {post} /point/ Create a new public/private point
+ * @apiVersion 0.1.0
+ * @apiName pointSubmit
+ * @apiGroup point
+ * @apiPermission private
+ *
+ * @apiDescription A signedin user can submit a new public/private point if the user's
+ * credit+bonus is more than 0.
+ *
+ * @apiParam {Decimal{10, 8}} lat Point's latitude
+ * @apiParam {Decimal{11, 8}} lng Point's longitude
+ * @apiParam {String{1..30}} name Point's name
+ * @apiParam {Number{11}} phone Point's phone number
+ * @apiParam {String{1..25}} province Point's province
+ * @apiParam {String{1..25}} city Point's city
+ * @apiParam {String{1..21844}} address Point's address
+ * @apiParam {Number=0,1} public Point is public/private
+ *
+ * @apiExample {json} Request-Example
+ *     {
+     *         "lat": "21.323",
+     *         "lng": "32.3343",
+     *         "name": "پیتزا آرشین",
+     *         "phone": "03155447658",
+     *         "province": "اصفهان",
+     *         "city": "کاشان",
+     *         "address": "خیابان امیرکبیر",
+     *         "public": "1"
+     *     }
+ *
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 201 Created
+ *
+ *
+ * @apiError (400) {lat} empty
+ * @apiError (400) {lat} not_valid_latitude Latitude should be a decimal number with maximum of 2 non-fractional and maximum of 8 fractional digits
+ *
+ * @apiError (400) {lng} empty
+ * @apiError (400) {lng} not_valid_longitude Longitude should be a decimal number with maximum of 3 non-fractional and 8 maximum of 8 fractional digits
+ *
+ * @apiError (400) {name} empty
+ * @apiError (400) {name} length_not_1_to_30
+ *
+ * @apiError (400) {phone} empty
+ * @apiError (400) {phone} not_numeric
+ * @apiError (400) {phone} length_not_11
+ *
+ * @apiError (400) {province} empty
+ * @apiError (400) {province} length_not_1_to_25
+ *
+ * @apiError (400) {city} empty
+ * @apiError (400) {city} length_not_1_to_25
+ *
+ * @apiError (400) {address} empty
+ * @apiError (400) {address} length_greater_than_21844
+ *
+ * @apiError (400) {public} empty
+ * @apiError (400) {public} not_0_or_1
+ *
+ * @apiError (400) not_enough_credit_bonus
+ *
+ * @apiError (404) owner_not_found This is a bug! if this error got returned sign out the user.
+ *
+ *
+ */
     .post(function (req, res, next) {
         req.validateBodyWithSchema(
             pointModel.schema,
@@ -21,6 +86,7 @@ router.route('/point')
 
                 pointModel.addPoint(point, function (err) {
                     if (!err) {
+                        // TODO: Return point's code
                         res.status(201).end();
                     }
                     else if (err === 'serverError') {
