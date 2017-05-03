2d3ad963d6f8e51dbe4c91c2c9e784288ef1414f
mahdavipanah <h.mahdavipanah@gmail.com>
2017-05-03 22:26:21 +0430

    Add SPLIT_STR function

---
+++ db/sql/create-functions.sql
@@ -0,0 +1,22 @@
+-- SPLIT_STR MySQL Function
+-- from http://blog.fedecarg.com/2009/02/22/mysql-split-string-function/
+
+CREATE FUNCTION SPLIT_STR(
+  x VARCHAR(255),
+  delim VARCHAR(12),
+  pos INT
+)
+RETURNS VARCHAR(255)
+RETURN REPLACE(SUBSTRING(SUBSTRING_INDEX(x, delim, pos),
+    LENGTH(SUBSTRING_INDEX(x, delim, pos -1)) + 1),
+    delim, '');
+
+/*
+Example:
+SELECT SPLIT_STR('a|bb|ccc|dd', '|', 3) as third;
++-------+
+| third |
++-------+
+| ccc   |
++-------+
+*/
