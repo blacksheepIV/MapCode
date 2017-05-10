f468927815c1ca4cf39d8fcf08fac880cedbe6cd
Forough Fallah <forough.fallahfar@gmail.com>
2017-05-10 09:56:46 +0430

    slight change in the view

--- public/js/controllers/pointInfoCtrl.js
+++ public/js/controllers/pointInfoCtrl.js
@@ -4,7 +4,7 @@
 function pointInfoCtrl ($scope,pointService,$mdDialog){
   $scope.initPoint = function(){
       $scope.point = pointService.getDetailedInfo();
-      console.log($scope.point);
+     // console.log($scope.point);
   };//end of function init Point
     $scope.cancel=function () {
         $mdDialog.cancel();
