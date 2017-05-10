f7253fdc062ca1a4626b9661a3f5eae0fcba8c0a
mahdavipanah <h.mahdavipanah@gmail.com>
2017-05-10 11:48:53 +0430

    Update `enforce_friendship_request_id_order` to new `friend_requests` table

--- db/sql/create-triggers.sql
+++ db/sql/create-triggers.sql
@@ -17,15 +17,15 @@ DELIMITER ;
 DELIMITER ~
 CREATE TRIGGER enforce_friendship_request_id_order BEFORE INSERT ON friend_requests
   FOR EACH ROW BEGIN
-    IF (NEW.requester = NEW.requestee)
+    IF (NEW.first_user = NEW.second_user)
     THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'SELF_REQUEST';
     END IF;
 
-    SET @lowerId := IF(NEW.requester < NEW.requestee, NEW.requester, NEW.requestee);
-    SET @higherId := IF(NEW.requester > NEW.requestee, NEW.requester, NEW.requestee);
-    SET NEW.requester = @lowerId;
-    SET NEW.requestee = @higherId;
+    SET @lowerId := IF(NEW.first_user < NEW.second_user, NEW.first_user, NEW.second_user);
+    SET @higherId := IF(NEW.first_user > NEW.second_user, NEW.first_user, NEW.second_user);
+    SET NEW.first_user = @lowerId;
+    SET NEW.second_user = @higherId;
   END;
 ~
 DELIMITER ;
