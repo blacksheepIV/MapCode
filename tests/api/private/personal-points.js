c7212db48d75921dc62513f31195047610490da3
mahdavipanah <h.mahdavipanah@gmail.com>
2017-05-11 11:38:01 +0430

    Add personal points tests

---
+++ tests/api/private/personal-points.js
@@ -0,0 +1,11 @@
+describe('POST /personal_points', function () {
+    it("Should have tests!");
+});
+
+describe('DELETE /personal_points/:code', function() {
+    it("Should have tests!");
+});
+
+describe('GET /personal_points', function () {
+    it("Should have tests!");
+});
